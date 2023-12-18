ccf8133c11dc51e54d632007e5858e81c0cb1db7
Alexandre Muraviov <amuraviov@globalia.ca>
2023-12-18 14:46:27 -0500

    simpify Leeroy's code

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -24,51 +24,51 @@ class GildedRose
 
     public function tick()
     {
-        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
 
-                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
+        if ($this->name === 'Sulfuras, Hand of Ragnaros') {
+            return;
+        }
+
+        if ($this->name === 'normal') {
+            $this->sellIn -= 1;
+            $this->quality -= 1;
+            
+            if ($this->sellIn <= 0) {
+                $this->quality -= 1;
             }
         }
 
-        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-            $this->sellIn = $this->sellIn - 1;
+        if ($this->name === 'Aged Brie') {
+            $this->sellIn -= 1;
+
+            if ($this->sellIn <= 0) {
+                $this->quality += 1;  
+            } 
+
+            $this->quality += 1;   
         }
 
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Aged Brie') {
-                if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
-            }
+        if ($this->name === 'Backstage passes to a TAFKAL80ETC concert') {
+            
+            $this->quality += 1;    
+
+            if ($this->sellIn <= 0) {
+                $this->quality = 0;  
+            } else if ($this->sellIn <= 5) {
+                $this->quality += 2;  
+            } else if ($this->sellIn <= 10) {
+                $this->quality += 1;  
+            } 
+
+            $this->sellIn -= 1;
+        }
+
+        if ($this->quality < 0) {
+            $this->quality = 0;
+        }
+
+        if ($this->quality > 50) {
+            $this->quality = 50;
         }
     }
 }
